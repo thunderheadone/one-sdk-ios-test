4292be202da4b48f0c34630db1ee8ee9eecc664f
Mon Zacharias <mzacharias@thunderhead.com>
2020-12-03 12:05:30 +0000

    updated

--- Package.swift
+++ Package.swift
@@ -14,8 +14,10 @@ let package = Package(
             targets: ["Thunderhead"]),
     ],
     targets: [
-        .binaryTarget(name: "Thunderhead",
-                      path: "Thunderhead.xcframework"
+        .binaryTarget(
+          name: "Thunderhead",
+          url: "https://github.com/thunderheadone/one-sdk-ios-test/Thunderheadx.xcframework.zip",
+          checksum: "651c52d90b3f84cfbcfe9579b1902aa873ffab747271b259635037eb8b705dd6"
         )
         
             
