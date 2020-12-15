974fbc6143e7539369991f0cc66f284118f3907c
Mon Zacharias <mzacharias@thunderhead.com>
2020-12-15 14:19:20 +0000

    updated

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "Thunderhead",
-            targets: ["Thunderhead", "ThunderheadBundle"]),
+            targets: ["Thunderhead"]),
     ],
     dependencies: [],
     targets: [
